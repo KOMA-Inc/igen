d4b60119f955393c8e22c8b109180884085e7ebd
gloomikon <gloomikon@jatapp.com>
2022-11-21 17:18:43 +0100

    Add project copy

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "igen",
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
+        .package(url: "https://github.com/jpsim/Yams.git", from: "5.0.1"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .executableTarget(
+            name: "igen",
+            dependencies: [
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                .product(name: "Yams", package: "Yams"),
+            ]),
+        .testTarget(
+            name: "igenTests",
+            dependencies: ["igen"]),
+    ]
+)
